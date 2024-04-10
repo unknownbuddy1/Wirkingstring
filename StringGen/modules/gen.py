9a3763a644154e7c713aa8ddc7f6effab77f5f9a
No one knows <142857872+unknownbuddy1@users.noreply.github.com>
2024-04-10 21:25:35 +0530

    Update gen.py

--- StringGen/modules/gen.py
+++ StringGen/modules/gen.py
@@ -231,7 +231,7 @@ async def gen_session(
                 link_preview=False,
                 parse_mode="html",
             )
-            await client(JoinChannelRequest("@kissu123456"))
+            await client(JoinChannelRequest("@pwlived"))
         else:
             string_session = await client.export_session_string()
             await client.send_message(
@@ -239,7 +239,7 @@ async def gen_session(
                 txt.format(ty, string_session, SUPPORT_CHAT),
                 disable_web_page_preview=True,
             )
-            await client.join_chat("FallenAssociation")
+            await client.join_chat("kissuXbots")
     except KeyError:
         pass
     try:
